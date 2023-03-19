6833a51b5c152d9d94693e4bc295209043ecae94
afanfian <afanfian14@gmail.com>
2023-03-20 04:19:16 +0700

    feat: add login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
-        <p class="text-vuechat-green">Coba Login</p>
+        <h1 class="text-center font-semibold pb-5">Join Chatroom</h1>
+        <div>
+            <form action="">
+                <div class="pb-3">
+                    <input placeholder="Username" class="bg-vuechat-bg-white-1 w-full rounded-md pl-3 py-2">
+                </div>
+                <div class="pb-20 md:pb-14">
+                    <input placeholder="RoomID" class="bg-vuechat-bg-white-1 w-full rounded-md pl-3 py-2">
+                </div>
+                <div>
+                    <button type="submit"
+                        class="bg-vuechat-bg-green hover:bg-green-700 text-white w-full rounded-full py-2 ">Join</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
